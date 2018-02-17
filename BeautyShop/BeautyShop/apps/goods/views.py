fef5e33653feb85d43dd1c1c3e7e94da4dc62d59
yangchengfeng <linmeinicoder@gmail.com>
2018-02-17 22:33:18 +0800

    对商品数据进行翻页设置

--- BeautyShop/BeautyShop/apps/goods/views.py
+++ BeautyShop/BeautyShop/apps/goods/views.py
@@ -5,12 +5,18 @@ from django.shortcuts import render
 from .serializers import GoodsSerializer
 from rest_framework import mixins
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Goods
 
 # Create your views here.
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    page_size_query_param = "p"
+    max_page_size = 4
+
 class GoodsListView(generics.ListAPIView):
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
+    pagination_class = StandardResultsSetPagination
