7d5d4c09a228ed7fd6d4a73fedf36220fdcac38b
yangchengfeng <linmeinicoder@gmail.com>
2018-02-17 22:26:18 +0800

    配置分页

--- BeautyShop/BeautyShop/BeautyShop/settings.py
+++ BeautyShop/BeautyShop/BeautyShop/settings.py
@@ -133,3 +133,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
+}
--- BeautyShop/BeautyShop/apps/goods/views.py
+++ BeautyShop/BeautyShop/apps/goods/views.py
@@ -3,25 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .serializers import GoodsSerializer
-from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Goods
 
 # Create your views here.
-class GoodsListView(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
-    
-    def post(self, request, format=None):
-        serializer = GoodsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class GoodsListView(generics.ListAPIView):
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
