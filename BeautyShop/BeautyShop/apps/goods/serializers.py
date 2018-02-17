993b0f18af0f90fda189744b20b8afc6198a8871
yangchengfeng <linmeinicoder@gmail.com>
2018-02-17 21:12:29 +0800

    添加post方法

--- BeautyShop/BeautyShop/apps/goods/serializers.py
+++ BeautyShop/BeautyShop/apps/goods/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 
+from goods.models import Goods
+
 class GoodsSerializer(serializers.Serializer):
     name = serializers.CharField(required=True, max_length=100)
     click_num = serializers.IntegerField(default=0)
-    sold_num = serializers.IntegerField(default=0)
     goods_front_image = serializers.ImageField()
+
+    def create(self, validated_data):
+        return Goods.objects.create(**validated_data)
--- BeautyShop/BeautyShop/apps/goods/views.py
+++ BeautyShop/BeautyShop/apps/goods/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from .serializers import GoodsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Goods
 
@@ -17,3 +18,10 @@ class GoodsListView(APIView):
         goods = Goods.objects.all()[:10]
         goods_serializer = GoodsSerializer(goods, many=True)
         return Response(goods_serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = GoodsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
